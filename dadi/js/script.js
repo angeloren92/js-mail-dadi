d3e68fb81895e31f98ed4b3c10056c75b11791d0
angeloren92 <angeloren92@gmail.com>
2026-02-05 15:33:54 +0100

    DADI start human code

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,1 +1,11 @@
 console.log('prova')
+// Traccia Dadi
+/* 
+-Dobbiamo generare dei numeri random (cercare in documentazione)
+-Molto probabilmente ci serviranno 2 array dove inserire 6 elementi a testa per i numeri dei dadi
+-Il giocatore che vince ha il numero maggiore, quindi forse ci serve un filtro 
+    con IF ed ELSE per paragonare i numeri e trovare il vincitore con numero maggiore
+-Possiamo provare a realizzare un ciclo collegato ad un valore booleano dove chiedere all'utente
+    se vuole giocare o meno
+
+*/
