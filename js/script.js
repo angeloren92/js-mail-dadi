568fc95a0f5422d5e922a43a1e3ad24fb11e29e2
angeloren92 <angeloren92@gmail.com>
2026-02-05 14:30:09 +0100

    MAIL add human code

--- js/script.js
+++ js/script.js
@@ -1,3 +1,28 @@
 console.log('test');
 
 // Traccia Email
+/* 
+-Abbiamo bisogno di un array che abbiamo come elementi le email degli invitati
+-Dobbiamo richiedere l'utente di inserire l'email
+-Dobbiamo dare l'istruzione di un ciclo che continua ad iterare con delle condizioni
+    .una condizione può essere che la lunghezza dell'array definisce il numero di iterazioni
+    .seconda condizione può essere che se trova l'email inserita si interrompe il ciclo
+    non ha necessità di continuare a cercare
+-Facciamo una selezione dell'email inserita confrontandola con gli elementi dell'array
+    .IF true - stampa messaggio ok
+    .ELSE false - stampa ulteriore messaggio
+
+-Ipotesi: non è necessario provvedere alla validazione della email, 
+    .ma i dati sono case sensitive, quindi per fare il controllo, trasformiamo i dati input in lowercase 
+
+tools:
+-array
+-prompt
+-for 
+-lenght
+-lowercase
+-if else
+-connettori logici
+-valori booleani
+-console.log
+*/
